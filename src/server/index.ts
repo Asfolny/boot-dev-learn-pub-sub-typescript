6d0bfa5214fa9ad9260951c9926a4e2ba41bf2be
Rasmus Frederiksen <rasmus@ufafarion.com>
2026-08-15 13:39:24 +0200

    Add rabbitmq connecting

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,14 @@
+import amqp from "amqplib";
+
 async function main() {
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(rabbitConnString);
+  console.log("Connection success");
   console.log("Starting Peril server...");
+  process.on("SIGINT", () => {
+    console.log("Shutting down connection to rabbitmq");
+    conn.close();
+  });
 }
 
 main().catch((err) => {
